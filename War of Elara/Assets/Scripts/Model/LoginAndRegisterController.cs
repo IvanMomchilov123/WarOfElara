d8c6428edaf49bcbd01d5cf436b670fae76d5111
DesislavPavlov <124437772+DesislavPavlov@users.noreply.github.com>
2024-02-23 19:31:27 +0200

    Add registration and login pt2
    Add unity authentication service and implemented it to a basic degree

--- War of Elara/Assets/Scripts/Model/LoginAndRegisterController.cs
+++ War of Elara/Assets/Scripts/Model/LoginAndRegisterController.cs
@@ -1,18 +1,129 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Authentication;
+using System.Threading.Tasks;
+using Unity.VisualScripting;
 using UnityEngine;
+using Unity.Services.Core;
+using Unity.Services.Authentication;
+using UnityEditor.UI;
+using UnityEngine.UI;
+using TMPro;
 
 public class LoginAndRegisterController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public TMP_InputField loginNameInput;
+    public TMP_InputField loginPasswordInput;
+    public TMP_InputField registerNameInput;
+    public TMP_InputField registerPasswordInput;
+
+    private string username;
+    private string password;
+
+    public string Username 
+    { 
+        get { return username; }
+        set 
+        {
+            if (value.Length < 3 || value.Length > 20)
+                this.username = "";
+
+            this.username = value;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public string Password
     {
-        
+        get { return password; }
+        set
+        {
+            if (value.Length < 8 || value.Length > 30)
+                this.password = "";
+
+            this.password = value;
+        }
     }
+
+    private async void Start()
+    {
+        try
+        {
+            await UnityServices.InitializeAsync();
+        }
+        catch (Exception ex)
+        {
+            Debug.LogException(ex);
+        }
+    }
+
+
+    private void SetValues(bool isRegistering)
+    {
+        if (isRegistering)
+        {
+            this.Username = registerNameInput.text;
+            this.Password = registerPasswordInput.text;
+        }
+        else
+        {
+            this.Username = loginNameInput.text;
+            this.Password = loginPasswordInput.text;
+        }
+    }
+
+    public async void Register()
+    {
+        SetValues(true);
+        await SignUpWithUsernamePassword(this.Username, this.Password);
+    }
+
+    public async void Login()
+    {
+        SetValues(false);
+        await SignInWithUsernamePasswordAsync(this.Username, this.Password);
+    }
+
+    private async Task SignUpWithUsernamePassword(string username, string password)
+    {
+        try
+        {
+            await AuthenticationService.Instance.SignUpWithUsernamePasswordAsync(username, password);
+            Debug.Log("SignUp is successful.");
+        }
+        catch (Unity.Services.Authentication.AuthenticationException ex)
+        {
+            // Compare error code to AuthenticationErrorCodes
+            // Notify the player with the proper error message
+            Debug.LogException(ex);
+        }
+        catch (RequestFailedException ex)
+        {
+            // Compare error code to CommonErrorCodes
+            // Notify the player with the proper error message
+            Debug.LogException(ex);
+        }
+    }
+
+    private async Task SignInWithUsernamePasswordAsync(string username, string password)
+    {
+        try
+        {
+            await AuthenticationService.Instance.SignInWithUsernamePasswordAsync(username, password);
+            Debug.Log("SignIn is successful.");
+        }
+        catch (Unity.Services.Authentication.AuthenticationException ex)
+        {
+            // Compare error code to AuthenticationErrorCodes
+            // Notify the player with the proper error message
+            Debug.LogException(ex);
+        }
+        catch (RequestFailedException ex)
+        {
+            // Compare error code to CommonErrorCodes
+            // Notify the player with the proper error message
+            Debug.LogException(ex);
+        }
+    }
+
 }
